2ba6fe63a744fedbd61b08547cb946c3ef7d7d3d
Kazım İncebay <kazimincebay@gmail.com>
2024-01-28 01:03:36 +0300

    First RabbitMQ Project is created.
    I created one solution and add two console application this solution. I can message from publisher to subscriber with RabbitMQ

---
+++ Hello World/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+};
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello-queue",true,false,false);
+
+string message = "hello world";
+
+var messageBody=Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty,"hello-queue",null,messageBody);
+
+Console.WriteLine("Mesaj Gönderilmiştir");
+Console.ReadKey();
---
+++ Hello World/RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "guest",
+    Password = "guest"
+};
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("hello-queue", true, consumer);
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Console.WriteLine("Gelen Mesaj " + message);
+};
+
+
+//channel.QueueDeclare("hello-queue", true, false, false);
+
+/* eğer ki QueueDeclare kısmını silersek publisher bu kuyruğu oluşturmadıysa hata verir. Eğer silmez isek publisher oluşturmadı ise subscriber tarafından kuyruk oluşturulmuş olur
+ bu nedenle hata vermez. Publisherın oluşturduğundan eminsek silinebilir, parametreleride aynı olmalıdır. */
+
+Console.ReadKey();
